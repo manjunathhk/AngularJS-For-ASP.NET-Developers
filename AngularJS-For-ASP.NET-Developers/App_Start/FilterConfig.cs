ff1d89a0ef7ff9aaf779e95c1bb5d3e9508b3d32
Manjunath HK <manjunathhk@gmail.com>
2015-04-24 15:41:01 +0530

    Initial/Basic

---
+++ AngularJS-For-ASP.NET-Developers/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AngularJS_For_ASP.NET_Developers
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ AngularJS-For-ASP.NET-Developers/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngularJS_For_ASP.NET_Developers.MvcApplication" Language="C#" %>
