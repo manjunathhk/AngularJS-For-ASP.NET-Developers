d926781a6bb7b35ca775a27919553f4015710205
Manjunath HK <manjunathhk@gmail.com>
2015-04-24 15:58:33 +0530

    Login page added

---
+++ AngularJS-For-ASP.NET-Developers/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularJS_For_ASP.NET_Developers.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
